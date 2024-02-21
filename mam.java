c7e2198341da23380d69b198198661ac2927694c
Admin <admin@Admins-MacBook-Pro-25.local>
2024-02-21 17:10:57 +0530

    fifth commit

--- mam.java
+++ mam.java
@@ -14,4 +14,10 @@ public class mam
         int value = sum/2;
         return value;
     }
+
+    public static int multiply(int value,int height)
+    {
+        int ans = value * height;
+        return ans;
+    }
 }
