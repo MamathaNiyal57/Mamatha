9fb8529ef7a4a56cfed7470773f62badab3982f2
Admin <admin@Admins-MacBook-Pro-25.local>
2024-02-21 17:08:04 +0530

    fourth commit

--- mam.java
+++ mam.java
@@ -8,4 +8,9 @@ public class mam
         int b=sc.nextInt();
         int height = sc.nextInt();
     }
+    public static int addition(int num1,int num2)
+    {
+        int sum = num1 + num2;
+        return sum;
+    }
 }
